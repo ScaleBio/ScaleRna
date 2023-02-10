8fe4407cdf04bb9d9c1fdc9e7c0b790d3368585f
Felix Schlesinger <felix.schlesinger@scale.bio>
2023-02-09 17:17:00 -0800

    Fix conda profile config

--- envs/download-scale-tools.sh
+++ envs/download-scale-tools.sh
@@ -3,5 +3,5 @@ srcDir=$(dirname -- "$0")
 dir=${1:-"$(readlink -f "${srcDir}/../bin")"}
 mkdir -p $dir
 echo "Downloading to $dir"
-curl http://scale.pub.s3.amazonaws.com/scaleTools/cicd/bcParser/master/221208-g4a82e54/bc_parser -o $dir/bc_parser && chmod 755 $dir/bc_parser
+curl http://scale.pub.s3.amazonaws.com/scaleTools/cicd/bcParser/master/230210-gcb42622/bc_parser -o $dir/bc_parser && chmod 755 $dir/bc_parser
 
